f780353a43198016dc162d58e506afc306bb3c2c
Samantha Blasbalg <blasbalgs@gmail.com>
2026-07-30 20:51:53 +0000

    chore: add storybook to the repo

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const storybook = require('eslint-plugin-storybook');
 
 module.exports = defineConfig([
   {
